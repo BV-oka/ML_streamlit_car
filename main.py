338c1f00e4568dcc5de3a38eeb912e4f08b6c5aa
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-19 16:48:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,3 +32,14 @@ if (status == 'да'):
     st.success("да")
 else:
     st.success("нет")
+
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+
+if genre == ':rainbow[Comedy]':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn\'t select comedy.")
