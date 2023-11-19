28e2cf351edb67971b98efb98d5124ffad972fa9
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-19 17:17:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ st.write('You selected:', option)
 
 
 # слайдеры
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+# age = st.slider('How old are you?', 0, 130, 25)
+# st.write("I'm ", age, 'years old')
 
 
 
