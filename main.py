f263c2369415e04dd2ff03d95496622058e0a623
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-19 14:26:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 # импортируем библиотеку streamlit
 import streamlit as st
+# импортируем библиотеку pandas
+import pandas as pd
 
 if st.button('Нажми на меня'):
   st.write('Привет, мир!')
+
+
+# Название
+st.title("Песочница")
+
+# Заголовок
+st.header("Это заголовок")
+
+# Подзаголовок
+st.subheader("Это подзаголовок")
+
+# Подзаголовок
+st.subheader("Это подзаголовок")
+
+# Текст
+st.text("Просто текст")
