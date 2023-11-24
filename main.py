7a131603f39d6e38db72b2b1ac98891aedf743c0
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-24 19:56:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,12 +33,21 @@ st.error("Error")
 # st.snow()
 # st.balloons()
 
+
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
 df
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
 
 
 # переключатели
