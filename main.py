bd1c0ac42c2d1d430deedda7047ac08b8628d32c
BV-oka <138525513+BV-oka@users.noreply.github.com>
2023-11-25 04:03:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,7 @@ import pandas as pd
 import numpy as np
 
 
-if st.button('Нажми на меня'):
-  st.write('Привет, мир!')
 
-
-# Название
-st.title("Песочница")
-
-# Заголовок
-st.header("Это заголовок")
-
-# Подзаголовок
-st.subheader("Это подзаголовок")
-
-# Подзаголовок
-st.subheader("Это подзаголовок")
-
-# Текст
-st.text("Просто текст")
 
 # полоски
 st.divider()
